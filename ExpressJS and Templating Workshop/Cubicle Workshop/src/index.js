0a8bed93e7edef4c5699da4437655e3bb5a255a5
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-09-22 18:22:23 +0300

    Your commit message here

--- ExpressJS and Templating Workshop/Cubicle Workshop/src/index.js
+++ ExpressJS and Templating Workshop/Cubicle Workshop/src/index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
+
 const app = express();
 const PORT = 3000;
 
+// Handlebars configuration.
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+app.set("views", "src/views");
+
 app.get("/", (req, res) => {
-  res.send("Hello from express server!");
+  res.render("index");
 });
 
 app.listen(PORT, () => console.log(`Server listening on port: ${PORT}...`));
