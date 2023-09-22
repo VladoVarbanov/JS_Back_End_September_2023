dd466a6af8f501658d476b031ce5da4d06f9b10e
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-09-22 12:38:58 +0300

    New tasks

--- ExpressJS and Templating/index.js
+++ ExpressJS and Templating/index.js
@@ -1,17 +1,80 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
 const app = express();
+const path = require("path");
 const PORT = 3000;
 
-// #region Routing
+const { getKittens, addKitten } = require("./kittens.js");
+
+// VIEW ENGINE
+app.engine("hbs", handlebars.engine({ extname: "hbs" }));
+app.set("view engine", "hbs");
+
+// #region MIDDLEWARE
+// third-party middleware.
+const bodyParser = express.urlencoded({ extended: false });
+app.use(bodyParser);
+
+const staticFile = express.static("public");
+app.use(staticFile);
+
+// global middleware.
+app.use((req, res, next) => {
+  console.log(`HTTP Request: ${req.method}, Request Path: ${req.path}`);
+  next();
+});
+
+//partial routing middleware.
+app.use("/kittens", (req, res, next) => {
+  console.log("Kittens Middleware has been invoked!");
+  next();
+});
+
+// concrete routing middleware.
+const specificMiddleware = (req, res, next) => {
+  console.log("This is the specific routes MIDDLEWARE");
+  next();
+};
+
+// #endregion MIDDLEWARE
+
+// #region ROUTING
 app.get("/", (req, res) => {
-  res.send("Welcome, this is the homepage!");
+  // res.send("Welcome, this is the homepage!");
+  res.render("home");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/specific", specificMiddleware, (req, res) => {
+  res.send("This is specific route! :)");
 });
 
 app.get("/kittens", (req, res) => {
-  res.send("Hello, kitty!");
+  const kittens = getKittens();
+  console.log({ kittens });
+  res.render("kittens", { kittens });
+});
+
+app.get("/kittens/:kittenId", (req, res) => {
+  const kittenId = Number(req.params.kittenId);
+  console.log(req.params);
+
+  if (!kittenId) {
+    res.status(404).send("Bad kitten id: " + req.params.kittenId);
+    return;
+  }
+
+  res.send({ id: kittenId, name: "Kircho" + kittenId });
 });
 
 app.post("/kittens", (req, res) => {
+  console.log(req.body);
+  const name = req.body.name;
+  const age = Number(req.body.age);
+  addKitten(name, age);
   res.send("Kitten has been created!");
 });
 
