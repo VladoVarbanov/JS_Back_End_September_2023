5755e75571f93ab31c0ce56e51e8f0f7760d949b
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-09-23 20:48:03 +0300

    CUB-P1 add bodyparser, provide cube create functionality, add cube template and foreach it

--- ExpressJS and Templating Workshop/Cubicle Workshop/src/controllers/cubeController.js
+++ ExpressJS and Templating Workshop/Cubicle Workshop/src/controllers/cubeController.js
@@ -1,7 +1,20 @@
 const router = require("express").Router();
+const cubeService = require("../services/cubeService.js");
 
 router.get("/create", (req, res) => {
+  // console.log(cubeService.getAll());
   res.render("create");
+});
+
+router.post("/create", (req, res) => {
+  const { name, description, imageUrl, difficultyLevel } = req.body;
+  cubeService.create({
+    name,
+    description,
+    imageUrl,
+    difficultyLevel: Number(difficultyLevel),
+  });
+  res.redirect("/");
 });
 
 module.exports = router;
