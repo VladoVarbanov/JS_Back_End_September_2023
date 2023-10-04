bcb15047bcde6ec6681aae8463e6d255c389745a
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-10-04 19:06:04 +0300

    CS- create handlebars configuration

--- Lab/Cat Shelter/src/index.js
+++ Lab/Cat Shelter/src/index.js
@@ -1,4 +1,6 @@
 const express = require("express");
 const expressConfig = require("./config/expressConfig.js");
+const handlebars = require("express-handlebars");
+const handlebarsConfig = require("./config/handlebarsConfig.js");
 
 const app = express();
