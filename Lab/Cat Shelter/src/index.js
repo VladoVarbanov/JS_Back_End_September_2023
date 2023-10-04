68bd21c8ff52be8677d62108f7e1b08b8ae17221
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-10-04 19:18:18 +0300

    CS- create and start server on port:3000

--- Lab/Cat Shelter/src/index.js
+++ Lab/Cat Shelter/src/index.js
@@ -3,4 +3,13 @@ const expressConfig = require("./config/expressConfig.js");
 const handlebars = require("express-handlebars");
 const handlebarsConfig = require("./config/handlebarsConfig.js");
 
+// ?Local variables
+const PORT = 3000;
 const app = express();
+
+//  !!! Configs !!!  //
+expressConfig(app);
+handlebarsConfig(app);
+
+//  !!!  SERVER  !!!  //
+app.listen(PORT, () => console.log(`Server listening on port: ${PORT}`));
