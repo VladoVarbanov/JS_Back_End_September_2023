7c0e12401ee802636e7ec8f08c465924e9d49bbf
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-10-08 20:14:18 +0300

    register userController, implement get endpoint for login and register

--- Session and Authentication Workshop/Cubicle Workshop/src/router.js
+++ Session and Authentication Workshop/Cubicle Workshop/src/router.js
@@ -2,10 +2,12 @@ const router = require("express").Router();
 const homeController = require("./controllers/homeController.js");
 const cubeController = require("./controllers/cubeController.js");
 const accessoryController = require("./controllers/accessoryController.js");
+const userController = require("./controllers/userController.js");
 
 router.use(homeController);
 router.use("/cubes", cubeController);
 router.use("/accessories", accessoryController);
+router.use("/users", userController);
 
 router.get("*", (req, res) => {
   res.redirect("/404");
