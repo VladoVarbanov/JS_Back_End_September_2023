942261925f7dab3045ed62d30ff3f45487d3237a
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-10-08 21:35:15 +0300

    Implement login logic bcrypt comparing

--- Session and Authentication Workshop/Cubicle Workshop/src/services/userService.js
+++ Session and Authentication Workshop/Cubicle Workshop/src/services/userService.js
@@ -1,4 +1,21 @@
 const User = require("../models/User.js");
+const bcrypt = require("bcrypt");
+
 exports.register = (userData) => {
   User.create(userData);
+};
+
+exports.login = async (username, password) => {
+  const user = await User.findOne({ username });
+  if (!user) {
+    throw new Error("Invalid username or password!");
+  }
+
+  const isValid = await bcrypt.compare(password, user.password);
+
+  if (!isValid) {
+    throw new Error("Invalid username or password!");
+  }
+
+  return user;
 };
