ae3f00c8f261fcde653bd0b6e678eb4ee6cac931
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-10-08 22:01:11 +0300

    Save the cookie jwt in browser to know you are logged in

--- Session and Authentication Workshop/Cubicle Workshop/src/config/expressConfig.js
+++ Session and Authentication Workshop/Cubicle Workshop/src/config/expressConfig.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const path = require("path");
+const cookieParser = require("cookie-parser");
 
 const expressConfig = (app) => {
   app.use(express.static(path.resolve(__dirname, "../public")));
   app.use(express.urlencoded({ extended: false }));
+  app.use(cookieParser());
 };
 
 module.exports = expressConfig;
--- Session and Authentication Workshop/Cubicle Workshop/src/services/userService.js
+++ Session and Authentication Workshop/Cubicle Workshop/src/services/userService.js
@@ -1,5 +1,7 @@
 const User = require("../models/User.js");
 const bcrypt = require("bcrypt");
+const jwt = require("../lib/jwt.js");
+const { SECRET } = require("../constants.js");
 
 exports.register = (userData) => {
   User.create(userData);
@@ -7,15 +9,22 @@ exports.register = (userData) => {
 
 exports.login = async (username, password) => {
   const user = await User.findOne({ username });
+  // Validate username.
   if (!user) {
     throw new Error("Invalid username or password!");
   }
 
+  // Validate password.
   const isValid = await bcrypt.compare(password, user.password);
-
   if (!isValid) {
     throw new Error("Invalid username or password!");
   }
 
-  return user;
+  const payload = {
+    _id: user._id,
+    username: user.username,
+  };
+  const token = await jwt.sign(payload, SECRET, { expiresIn: "3d" });
+
+  return token;
 };
