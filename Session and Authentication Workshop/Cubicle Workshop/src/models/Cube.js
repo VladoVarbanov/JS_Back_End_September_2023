b725e5847e997de10c4ba489f354d93f70c3206e
VladoVarbanov <116281666+VladoVarbanov@users.noreply.github.com>
2023-10-11 19:05:28 +0300

    Update cube schema, adding user reference

--- Session and Authentication Workshop/Cubicle Workshop/src/models/Cube.js
+++ Session and Authentication Workshop/Cubicle Workshop/src/models/Cube.js
@@ -5,6 +5,10 @@ const cubeSchema = new mongoose.Schema({
   description: String,
   imageUrl: String,
   difficultyLevel: Number,
+  owner: {
+    type: mongoose.Types.ObjectId,
+    ref: "User",
+  },
 });
 
 const Cube = mongoose.model("Cube", cubeSchema);
